d4fc46179e2130d09b4aef7d4444a2d942b1210a
swagath shetty <swagath.shetty@gmail.com>
2022-02-25 19:51:21 +0530

    added basic add posts functionality

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,9 +1,17 @@
 import {useState} from 'react'
-
+import axios from 'axios'
 const PostCreate=()=>{
     const [title,setTitle]=useState('')
-    const onSubmit=()=>{
-        
+
+    const onSubmit=async (event)=>{
+        event.preventDefault()
+
+        await axios.post('http://localhost:4000/posts',{
+            title
+        })
+
+        setTitle('')
+
     }
     return <div>
         <form onSubmit={onSubmit}>
