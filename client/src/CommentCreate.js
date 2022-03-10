de442b183a26aa5136cbbc5d68958fbceeaf8662
swagath shetty <swagath.shetty@gmail.com>
2022-03-10 20:16:15 +0530

    integrated comment create api in client

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,9 +1,20 @@
 import React,{useState} from "react";
+import axios from 'axios'
 
 const CommentCreate=({postId})=>{
     const [content,setContent]=useState('')
+    
+    const onSubmit=async(event)=>{
+        event.preventDefault()
+
+        axios.post(`http://localhost:4001/posts/${postId}/comments`,{
+            content
+        })
+
+        setContent('')
+    }
     return <div>
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="form-group">
              <label>New comment</label>    
              <input value={content} onChange={(e)=>setContent(e.target.value)} className="form-control" />
