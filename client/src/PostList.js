302af915f347cd9f60076362b35a4eee03d98e08
swagath shetty <swagath.shetty@gmail.com>
2022-03-16 19:40:13 +0530

    added comment list component

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,7 @@
 import { useState,useEffect } from "react"
 import axios from "axios"
 import CommentCreate from "./CommentCreate"
+import CommentList from "./CommentList"
 const PostList=()=>{
     const [posts,setPosts]=useState({})
 
@@ -19,6 +20,7 @@ const PostList=()=>{
             <div className="card-body">
                 <h3>{post.title}</h3>
                 <CommentCreate postId={post.id} />
+                <CommentList postId={post.id} />
             </div>
         
         </div>
