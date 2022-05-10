a5932cf627786cdfda12e743187964962bbfcece
swagath shetty <swagath.shetty@gmail.com>
2022-05-10 19:48:16 +0530

    initalised comment moderation service

--- query/index.js
+++ query/index.js
@@ -18,11 +18,11 @@ app.post('/events',(req,res)=>{
         const {id,title}=data
         posts[id]={id,title,comments:[]}
     }else if(type==='CommentCreated'){
-        const {id,content,postId}=data
+        const {id,content,postId,status}=data
 
         const post=posts[postId]
 
-        post.comments.push({id,content})
+        post.comments.push({id,content,status})
     }
     console.log(posts)
     res.send({})
