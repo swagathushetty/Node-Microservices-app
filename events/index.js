8bd78e1b44da40469077ca66bd6171ccbfca5d60
swagath shetty <swagath.shetty@gmail.com>
2022-05-12 17:22:54 +0530

    successfully fixed all issues

--- events/index.js
+++ events/index.js
@@ -1,28 +1,28 @@
-const express=require('express')
-const bodyParser=require('body-parser')
-const axios=require('axios')
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
-const app=express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-app.post('/events',async(req,res)=>{
-    const event=req.body
-    try {
-        
-    await axios.post('http://localhost:4000/events',event)
-    await axios.post('http://localhost:4001/events',event)
-    await axios.post('http://localhost:4002/events',event)
-    await axios.post('http://localhost:4003/events',event)
+app.post("/events", (req, res) => {
+  const event = req.body;
 
-    res.send({status:'OK'})
-    }catch(err){
-        console.log(err)
-    }
-    
-})
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  res.send({ status: "OK" });
+});
 
-
-
-app.listen(4005,()=>{
-    console.log('listening on 4005')
-})
+app.listen(4005, () => {
+  console.log("Listening on 4005");
+});
--- query/index.js
+++ query/index.js
@@ -1,33 +1,53 @@
-const express=require('express')
-const bodyParser=require('body-parser')
-const cors=require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app=express()
-app.use(bodyParser.json())
-app.use(cors())
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const posts={}
+const posts = {};
 
-app.get('/posts',(req,res)=>{
-    res.send(posts)
-})
-
-app.post('/events',(req,res)=>{
-    const {type,data}=req.body
-    if(type==='PostCreated'){
-        const {id,title}=data
-        posts[id]={id,title,comments:[]}
-    }else if(type==='CommentCreated'){
-        const {id,content,postId,status}=data
-
-        const post=posts[postId]
-
-        post.comments.push({id,content,status})
-    }
+app.get('/posts', (req, res) => {
     console.log(posts)
-    res.send({})
-})
+  res.send(posts);
+});
 
-app.listen(4002,()=>{
-    console.log('listening on 4002')
-})
+app.post('/events', (req, res) => {
+
+    console.log('rannn')
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    console.log('21',posts)
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content, status });
+  }
+
+  if (type === 'CommentUpdated') {
+    const { id, content, postId, status } = data;
+
+    const post = posts[postId];
+    const comment = post.comments.find(comment => {
+      return comment.id === id;
+    });
+
+    comment.status = status;
+    comment.content = content;
+  }
+
+  console.log('44',posts);
+
+  res.send({});
+});
+
+app.listen(4002, () => {
+  console.log('Listening on 4002');
+});
